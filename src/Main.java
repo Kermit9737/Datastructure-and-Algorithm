f14bf671cf4513aeae2dfe4344c090cf2bbf7446
Kermit9737 <kermit9737@163.com>
2020-07-24 11:02:32 +0800

    Implemented the "LinkedList" and "BST" of Set

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
+import setandmap.FileOp;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello github!");
-        System.out.println("How are you!");
-        System.out.println("Really?");
-        System.out.println("ok...");
-        System.out.println("Wait!");
+        ArrayList<String> words = new ArrayList<>();
+        FileOp.readFile("a-tale-of-two-cities.txt",words);
+        System.out.println(words.size());
     }
 }
